040173ab3b0012811b46a42dc34c915fb3d51108
Misha Krasnonos <muzishutka7@gmail.com>
2021-11-07 23:19:58 +0200

    up

--- express-server.js
+++ express-server.js
@@ -6,12 +6,13 @@ const app = express()
 const {router} = require("./Router")
 require("dotenv").config()
 const e = process.env
-
+const cors = require('cors')
+ 
 // app.use(express.json())
 // app.use(express.urlencoded({extended: true}))
 // app.use(express.static('public'))
 app.use(morgan('tiny'))
-
+app.use(cors())
 // app.use('/api', router)
 
 const PORT = e.PORT
